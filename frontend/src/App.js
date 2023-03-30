daa4443c96ad0b0f530276dab242786de281908e
Your Name <you@example.com>
2023-03-30 17:01:11 +0200

    make change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import './App.css';
 import Home from './components/Home.jsx'
+import { Login } from "./components/pages/Login";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home/> } />
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </div>
   );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <h1 className='underline font-bold text-3xl'>Hello</h1>
+      <h1 className='underline font-bold text-3xl'>Hello Devo its now working!!!!!!</h1>
     </div>
   )
 }
