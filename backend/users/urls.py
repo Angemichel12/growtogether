e36df97af1c2c3e8e83ebaa92cd747b24f07864e
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-04-03 15:22:17 +0200

    create user registration

---
+++ backend/users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import UserRegister
+
+urlpatterns = [
+    path('register/', UserRegister.as_view()),
+]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from .serializers import UserRegisterSerializer
+from rest_framework.views import APIView
+from shortcuts.auto_generate import auto_username_password_generator
+class UserRegister(APIView):
+	permission_classes = (permissions.AllowAny,)
+	def post(self, request):
+		clean_data = auto_username_password_generator(request.data)
+		serializer = UserRegisterSerializer(data=clean_data)
+		if serializer.is_valid(raise_exception=True):
+			user = serializer.create(clean_data)
+			if user:
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(status=status.HTTP_400_BAD_REQUEST)
