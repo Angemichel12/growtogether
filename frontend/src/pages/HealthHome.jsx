01bfa75f927e435bc60e7fba293a861df920a0ff
Your Name <you@example.com>
2023-04-06 09:23:01 +0200

    my done

--- frontend/src/pages/HealthHome.jsx
+++ frontend/src/pages/HealthHome.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import DoctorNav from '../components/DoctorNav'
-
+import TypeWriterEffect from 'react-typewriter-effect';
 const HealthHome = () => {
     return (
         <div>
-            <div className='healthbg'>
+            <div >
                 <DoctorNav />
+                <div className='healthbg'>
+                    <span className='flex justify-center pt-60 '>
+
+                        <TypeWriterEffect
+                            textStyle={{
+                                color: 'white',
+                                fontWeight: 700,
+                                fontSize: '8rem',
+                            }}
+                            cursorColor="gray"
+                            cursorSize="0.1px"
+                            multiText={['GROW2GETHER']}
+                            typeSpeed={300}
+                            multiTextDelay={1000}
+                            multiTextLoop
+                        />
+                    </span>
+                </div>
             </div>
         </div>
     )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,84 +8,86 @@ import { Link } from 'react-router-dom';
 
 
 const Login = () => {
-    const [showPassword, setShowPassword] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [isChecked, setIsChecked] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const handleShowPassword = () => {
-        setShowPassword(!showPassword);
-      
-    };
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+
+  };
   const handleCheckboxChange = (event) => {
     setIsChecked(event.target.checked);
   };
-  const handleLoginClick = () => {};
+  const handleLoginClick = () => { };
 
-    return (
-        <div>
-            <div>
-            <Header/>
-            </div>
-            <div className="login h-full py-4 text-xl font-normal">
-            <form className=" rounded-3xl justify-center content-center max-w-xs mx-auto  p-4 bg-white  " onSubmit={handleSubmit}>
-                <h1 className="font-semibold text-3xl text-center justify-center text-black ">WELCOME</h1><br/>
-                <label htmlFor="email">Email</label><br></br>
-      <input
-        type="email"
-        className="border w-full rounded-xl bg-gray-200"
-        id="email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-        required
-      /><br/>
-
-      <label htmlFor="password">Password</label>
-      <div className=" flex relative">
-        <input
-          className="border w-full rounded-xl bg-gray-200"
-          type={showPassword ? "text" : "password"}
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          required
-        />
-         <span
-          className="password-icon absolute top-1 right-2 "
-          onClick={handleShowPassword}
-          aria-label="Toggle password visibility">
-          {showPassword ?<FaEye />: <FaEyeSlash />}
-        </span>
-       
+  return (
+    <div>
+      <div>
+        <Header />
       </div>
-      <a href="#" className="underline text-sm">Forget password?</a><br/>
-    
-        <input
-         type="checkbox" 
-         id="terms-and-conditions"
-         checked={isChecked}
-         onChange={handleCheckboxChange} />
-         <label for="terms" className=" text-xs"> I agree to Terms and contitions</label><br/><br/>
-         <div className="border w-28 mx-auto">
-         <Link to="/Umubyeyidash" >  <button 
-            disabled={!isChecked} 
-            onClick={handleLoginClick} 
-            className="bg-green-800 border-2 rounded-3xl content-center px-4 w-28">
-                Login
+      <div className="login h-full py-4 text-xl font-normal">
+        <form className=" rounded-3xl flex flex-col justify-center items-center  mx-auto h-5/6 p-4 bg-white w-4/12 " onSubmit={handleSubmit}>
+          <h1 className="font-semibold text-3xl text-center justify-center text-black mb-4">WELCOME</h1>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="border w-fit p-1 rounded-xl bg-gray-200"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          /><br />
+
+          <label htmlFor="password">Password</label>
+          <div className=" flex relative">
+            <input
+              className="border w-fit p-1 rounded-xl bg-gray-200"
+              type={showPassword ? "text" : "password"}
+              id="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+            />
+            <span
+              className="password-icon absolute top-2 right-2 "
+              onClick={handleShowPassword}
+              aria-label="Toggle password visibility">
+              {showPassword ? <FaEye /> : <FaEyeSlash />}
+            </span>
+
+          </div>
+          <Link to="#" className="underline text-sm mt-4">Forget password?</Link><br />
+
+          <div className='mb-8' >
+            <input
+              type="checkbox"
+              id="terms-and-conditions"
+              checked={isChecked}
+              onChange={handleCheckboxChange} />
+            <label for="terms" className=" text-xs "> I agree to Terms and contitions</label>
+          </div>
+          <div className=" mx-auto">
+            <Link to="/Umubyeyidash" >  <button
+              disabled={!isChecked}
+              onClick={handleLoginClick}
+              className="bg-green-800 rounded-3xl content-center px-4 w-28 text-white p-2">
+              Login
             </button></Link>
-        </div>
+          </div>
         </form>
-            </div>
-            <div>
-            <Footer/>
-            </div>
-        </div>
-    )
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  )
 }
 
 export default Login
