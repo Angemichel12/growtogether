8e2cdce1ee1501fa4b17715e14f59f1305fb336e
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-04-25 16:25:20 +0200

    Update settings.py

--- backend/growtogether/settings.py
+++ backend/growtogether/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=3_q68zs&2gt%tf%4y+(aav++y98nx9@rgeqcwt*ld9($_!8wv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['localhost','MichelNasri.www.pythonanywhere.com']
