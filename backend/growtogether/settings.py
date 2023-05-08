8ea7198732bec1a6575ec05fa83962726c8fd99e
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-05-08 16:52:49 +0200

    Update settings.py

--- backend/growtogether/settings.py
+++ backend/growtogether/settings.py
@@ -15,7 +15,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
-PRODUCTION = int(os.environ.get("PRODUCTION", default=1))
 ALLOWED_HOSTS = ['*']
 
 
