5ee0d7903e50e2b8538a68382a9fbe3ee4ef5a6f
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-05-11 14:52:36 +0200

    Delete backend/doctor directory

--- backend/doctor/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import doctor
-
-class doctorAdmin(admin.ModelAdmin):
-    list_display=['get_full_name','department', 'get_phone', 'user']
-
-admin.site.register(doctor,doctorAdmin)
