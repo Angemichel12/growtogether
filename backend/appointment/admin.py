171b584ee7303fffc2c376d582f3f4eff615fbbd
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-05-11 14:51:17 +0200

    Delete backend/appointment directory

--- backend/appointment/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import SemesterAppointment, Vaccination, Appointment
-
-
-admin.site.register(SemesterAppointment)
-admin.site.register(Vaccination)
-admin.site.register(Appointment)
