96c1ef1142c6f5279cd5e5034ed1681d1d6e44eb
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-05-15 08:24:56 +0200

    add cors_origin_whitelist

--- backend/growtogether/settings.py
+++ backend/growtogether/settings.py
@@ -182,6 +182,12 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     'https://growtogether-production.up.railway.app',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1:',
+    'https://growtogether-production.up.railway.app',
+    'http://localhost:3000',
+]
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
