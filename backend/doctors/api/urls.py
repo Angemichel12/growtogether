3269146d02be04e4c97b2946fd3045bb9494303f
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-05-15 14:59:54 +0200

    create verification email for Doctor

--- backend/doctors/api/urls.py
+++ backend/doctors/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/', views.DoctorsAPIView.as_view()),
     path('login/', views.CustomAuthToken.as_view()),
     path('logout/', views.User_logout),
+    path('doctor_activation/', views.VerifyAccount.as_view(), name='doctor-verify')
 ]
--- backend/doctors/api/views.py
+++ backend/doctors/api/views.py
@@ -15,6 +15,14 @@ from users.utils import Util
 from django.urls import reverse
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from django.conf import settings
+import jwt
+
+
+User = get_user_model()
+
 
 class CustomAuthToken(ObtainAuthToken):
 
@@ -85,7 +93,7 @@ class DoctorsAPIView(APIView):
         if user:
                     token= RefreshToken.for_user(user).access_token 
                     current_site= get_current_site(request).domain 
-                    rela_link= reverse('email-verify')        
+                    rela_link= reverse('doctor-verify')        
                     abs_url= 'http://'+current_site +rela_link+'?token='+str(token)
                     email_body= 'Hello Dr. '+ user.first_name+'.\n\nYour Account has been Created Successfully!\n\nUse the link provided below to activate your account.\n'+ abs_url
                     data= {
@@ -96,3 +104,32 @@ class DoctorsAPIView(APIView):
                     return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(status=status.HTTP_400_BAD_REQUEST)
+class VerifyAccount(generics.GenericAPIView):
+    def get(self, request):
+        token= request.GET.get('token') 
+        
+        try:
+            payload= jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+            user= User.objects.get(id=payload['user_id'])
+            
+            if not user.is_active:
+                user.is_active= True                             
+                user.save()
+                
+                current_site= get_current_site(request).domain 
+                rela_link= reverse('login')        
+                abs_url= 'http://'+current_site +rela_link
+                
+                email_body= 'Hello Dr. '+ user.first_name+'.\n\nYour Account is successfully activated!\n\n Use Credentials provided below to login into your account.\n\n' +'\n\nLogin Link:\n'+ abs_url
+                data= {
+                    'email_body': email_body,
+                    'to_email': user.email,
+                    'email_subject': 'Login into your Growtogether account.'}
+                Util.send_email(data)
+                # send_sms(request, user.phone)                
+            return Response({'Email is Verified': 'Your Account is successfully activated!'}, status= status.HTTP_200_OK)
+        
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'It\'s an Error':'Activation link expired!'}, status= status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'It\'s an Error':'Invalid token!'}, status= status.HTTP_400_BAD_REQUEST)
