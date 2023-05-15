8d681af3c17b1847d48030b2621bd142b738a4b1
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-05-15 12:10:54 +0200

    create doctor verification email

--- backend/doctors/api/views.py
+++ backend/doctors/api/views.py
@@ -10,7 +10,11 @@ from auto_tasks.auto_generate import fullname_generator
 from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth import logout
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework_simplejwt.tokens import RefreshToken
+from users.utils import Util
+from django.urls import reverse
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.hashers import make_password
 
 class CustomAuthToken(ObtainAuthToken):
 
@@ -68,7 +72,27 @@ class DoctorsAPIView(APIView):
     def post(self, request, format=None):
         clear_data = request.data
         clear_data=fullname_generator(clear_data)
+        password = clear_data.get('password')
+        confirm_password = clear_data.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            return Response({"error": "Passwords do not match."}, status=status.HTTP_400_BAD_REQUEST)
+
+        clear_data['password']= make_password(clear_data['password'])
         serializer = RegisterDoctorSerializer(data=clear_data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        user = serializer.save()
+        if user:
+                    token= RefreshToken.for_user(user).access_token 
+                    current_site= get_current_site(request).domain 
+                    rela_link= reverse('email-verify')        
+                    abs_url= 'http://'+current_site +rela_link+'?token='+str(token)
+                    email_body= 'Hello Dr. '+ user.first_name+'.\n\nYour Account has been Created Successfully!\n\nUse the link provided below to activate your account.\n'+ abs_url
+                    data= {
+                        'email_body': email_body,
+                        'to_email': user.email,
+                        'email_subject': 'Activate Your Account on Growtogether system.'}
+                    Util.send_email(data)
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
