2453b36c13da72d0b5a7e64ec7769bffc0c4eff9
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-05-16 10:29:28 +0200

    create post method for user registration

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,18 +2,16 @@ from django.urls import path, include
 from .views import (UserRegister, VerifyAccount,
                      ChangePasswordApi, 
                      LoginApi, LogoutApi,
-                     ResetPasswordApi)
-
-from rest_framework.routers import DefaultRouter
-
+                     ResetPasswordApi,
+                     UserRegister,)
 
 
-router= DefaultRouter()
-router.register('users', UserRegister, basename='users')
+
+
 
 urlpatterns = [ 
             
-    path('', include(router.urls)),    
+    path('users/',UserRegister.as_view(), name="user_register"),    
     path('activateaccount/', VerifyAccount.as_view(), name='email-verify'),
     path('login/', LoginApi.as_view(), name='login'),
     path('logout/', LogoutApi.as_view(), name='logout'),
