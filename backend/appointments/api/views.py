fdbcb7d7d036f1de79fb70f9845d0a8c6003537c
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-05-16 16:11:52 +0200

    create put, delete and post method

--- backend/appointments/api/views.py
+++ backend/appointments/api/views.py
@@ -3,7 +3,7 @@ from .serializers import WriteAppointmentSerializer, ReadAppointmentSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from appointments.models import Appointment
-
+from django.http import Http404
 
 class UserAppointmentAPIView(APIView):
 
@@ -20,3 +20,28 @@ class UserAppointmentAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def get_object(self, pk):
+        try:
+            return Appointment.objects.get(pk=pk)
+        except Appointment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        appointment = self.get_object(pk)
+        serializer = ReadAppointmentSerializer(appointment)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        appointment = self.get_object(pk)
+        serializer = WriteAppointmentSerializer(appointment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        appointment= self.get_object(pk)
+        appointment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
